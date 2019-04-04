feb5578ed2edcf11d3a192cad164056448e3b75d
kachow6 <kevinchow06@gmail.com>
2019-04-03 23:01:46 -0700

    Removed option to close modal

--- static/game.js
+++ static/game.js
@@ -166,15 +166,3 @@ function getMousePos(canvas, evt) {
         y: (evt.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height
     };
 }
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
